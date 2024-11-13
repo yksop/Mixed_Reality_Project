e3c34411292fdbc59098a5a3efacb71da9b742b9
filippoavi <filippoavi@github.me>
2024-11-13 17:29:22 +0100

    Implemented PlayerUpdatePosition and PlayerUpdateRotation

--- HL_Control_Android/Assets/Scripts/PlayerVisualizer.cs
+++ HL_Control_Android/Assets/Scripts/PlayerVisualizer.cs
@@ -7,20 +7,23 @@ This class is resposnsible for visualizing the position/rotation of the player u
 */
 public class PlayerVisualizer : MonoBehaviour
 {
+    Vector3 position;
+    Quaternion rotation;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = this.transform.gameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayerUpdatePosition(byte[] posB)
     {
-        
+        Vector3 pos = new Vector3(posB[1], posB[2], posB[3]);
+        player.transform.position = pos;
     }
-
-    public void PlayerUpdatePosition()
+    public void PlayerUpdateRotation(byte[] rotB)
     {
-
+        Quaternion rot = new Quaternion(rotB[1], rotB[2], rotB[3], rotB[4]);
+        player.transform.rotation = rot;
     }
 }
