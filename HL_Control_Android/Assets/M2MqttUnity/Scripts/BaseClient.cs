35fc86b4763340a3f1602ce09bb401b74ddff177
filippoavi <filippoavi@github.me>
2024-12-10 13:07:11 +0100

    Auto stash before merge of "main" and "origin/main"
    WIP mosquitto connection and update

--- HL_Control_Android/Assets/M2MqttUnity/Scripts/BaseClient.cs
+++ HL_Control_Android/Assets/M2MqttUnity/Scripts/BaseClient.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
--- HL_Control_Android/Assets/Scripts/PlayerVisualizer.cs
+++ HL_Control_Android/Assets/Scripts/PlayerVisualizer.cs
@@ -10,20 +10,59 @@ public class PlayerVisualizer : MonoBehaviour
     Vector3 position;
     Quaternion rotation;
     public GameObject player;
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = this.transform.gameObject;
-    }
+    public GameObject avatar;
+    //public Material lineMaterial;
+    private Color lineColor = Color.green;
 
     public void PlayerUpdatePosition(byte[] posB)
     {
         Vector3 pos = new Vector3(posB[1], posB[2], posB[3]);
-        player.transform.position = pos;
+        player.transform.position = pos * 20;
     }
     public void PlayerUpdateRotation(byte[] rotB)
     {
         Quaternion rot = new Quaternion(rotB[1], rotB[2], rotB[3], rotB[4]);
         player.transform.rotation = rot;
     }
+
+    public void AvatarUpdatePosition(byte[] posB)
+    {
+        Vector3 pos = new Vector3(posB[1], posB[2], posB[3]);
+        avatar.transform.position = pos * 20;
+    }
+    public void AvatarUpdateRotation(byte[] rotB)
+    {
+        Quaternion rot = new Quaternion(rotB[1], rotB[2], rotB[3], rotB[4]);
+        avatar.transform.rotation = rot;
+    }
+    /*
+    public void DrawRoom(List<Vector3> points)
+    {
+        for (int i = 0; i < points.Count - 1; i++)
+        {
+            GameObject myLine = new GameObject();
+            myLine.transform.position = points[i];
+            LineRenderer lr = myLine.AddComponent(typeof(LineRenderer)) as LineRenderer;
+            lr.material = lineMaterial;
+            lr.startColor = lineColor;
+            lr.endColor = lineColor;
+            lr.startWidth = 0.1f;
+            lr.endWidth = 0.1f;
+            lr.SetPosition(0, points[i]);
+            lr.SetPosition(1, points[i+1]);
+        }
+    }
+    public void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.2f)
+    {
+        GameObject myLine = new GameObject();
+        myLine.transform.position = start;
+        LineRenderer lr = myLine.AddComponent(typeof(LineRenderer)) as LineRenderer;
+        lr.material = lineMaterial;
+        lr.SetColors(color, color);
+        lr.SetWidth(0.1f, 0.1f);
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        GameObject.Destroy(myLine, duration);
+    }
+    */
 }
