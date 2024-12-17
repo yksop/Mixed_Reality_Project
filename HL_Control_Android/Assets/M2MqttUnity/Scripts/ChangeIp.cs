ce8d73f2dbff101e2386a8f495fd8c01a85a213d
filippoavi <filippoavi@github.me>
2024-12-17 15:12:38 +0100

    Android app broker IP change di nuovo

---
+++ HL_Control_Android/Assets/M2MqttUnity/Scripts/ChangeIp.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using M2MqttUnity;
+using TMPro;
+
+public class ChangeIp : MonoBehaviour
+{
+    public BaseClient baseClient;
+    public GameObject textInputGo;
+
+    public void ActivateInput()
+    {
+        textInputGo.SetActive(true);
+    }
+    public void DeactivateInput()
+    {
+        Debug.Log("Pressed");
+        string text = textInputGo.GetComponentInChildren<TMP_InputField>().text;
+        baseClient.BCChangeIp(text);
+        textInputGo.SetActive(false);
+    }
+}
