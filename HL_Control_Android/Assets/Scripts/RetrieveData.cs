48f055656332e2b611760d5b97b3d286ea704008
tommy <tommytoniato@gmail.com>
2024-12-28 14:54:19 +0100

    plot grafici con scritte

--- HL_Control_Android/Assets/Scripts/RetrieveData.cs
+++ HL_Control_Android/Assets/Scripts/RetrieveData.cs
@@ -68,6 +68,7 @@ public class RetrieveData : MonoBehaviour
     private Vector2 previousPosition;
     public float playerSpeed; // Velocità corrente
     public List<float> playerSpeeds = new List<float>(); // Storico velocità
+    public List<float> playerDistances = new List<float>(); // Storico distanze
 
     // Start is called before the first frame update
     void Start()
@@ -87,13 +88,16 @@ public class RetrieveData : MonoBehaviour
 
         float distance = Vector2.Distance(playerTransform.position, robotVirtual.position) / FATTORE_DI_SCALA;
         textDistanza.text = $"Distanza: {distance:F2} m";
+
+        // Salva la distanza nella lista
+        //playerDistances.Add(distance);
     }
 
     private IEnumerator UpdatePlayerSpeedRoutine(float updateInterval = 1f)
     {
         while (true)
         {
-            if (playerTransform != null)
+            if (playerTransform != null && robotVirtual != null)
             {
                 playerSpeed = (Vector2.Distance(playerTransform.position, previousPosition)) / FATTORE_DI_SCALA;
                 textVelocita.text = $"Velocità: {playerSpeed:F2} m/s";
@@ -101,9 +105,24 @@ public class RetrieveData : MonoBehaviour
 
                 // Aggiungi la velocità corrente alla lista
                 playerSpeeds.Add(playerSpeed);
+
+                // Calcola la distanza
+                float distance = Vector2.Distance(playerTransform.position, robotVirtual.position) / FATTORE_DI_SCALA;
+
+                playerDistances.Add(distance);
             }
 
+
+
             yield return new WaitForSeconds(updateInterval); // Update every second
+
+
         }
     }
+    
+
+
+
+
+
 }
