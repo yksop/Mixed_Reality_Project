d67cfc77bee83389ab4795625d6dcba86f85ec38
Giacomo Bianchi <giacomo.bianchi-2@studenti.unitn.it>
2025-01-15 19:50:05 +0100

    comments in english

--- HL_Control_Android/Assets/Scripts/RetrieveData.cs
+++ HL_Control_Android/Assets/Scripts/RetrieveData.cs
@@ -1,3 +1,8 @@
+/// <summary>
+/// The RetrieveData class is responsible for tracking and displaying the player's speed and distance 
+/// relative to a virtual robot in a mixed reality environment. It updates these values at regular intervals 
+/// and provides functionality to reset the data and clear graphical elements from the scene.
+/// </summary>
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
